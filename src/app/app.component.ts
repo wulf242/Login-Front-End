6f40f16ce50560475aa1b5636def2afd9b45b3dd
samuel turner Guernsey <34995555+wulf242@users.noreply.github.com>
2021-01-31 11:34:35 -0500

    this is done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'Schnell-Schnell';
+  public email = ''
+  public password = ''
+  private token = ''
+  public invalid = ''
+
+  
+gernerateToken(){
+  var time = new Date();
+  var hours = time.getHours().toString();
+  var minuets = time.getMinutes().toString();
+  let token: string = hours + minuets;
+  return token;
 }
+
+constructor(
+  private httpClient: HttpClient
+){}
+
+async validate () {
+ var code =  await this.httpClient.get<string>("/api/validate").toPromise()
+ if(code == this.token){
+   document.location.href = 'http://onecause.com'
+ }
+ else {
+   this.invalid = "wrong username or password"
+ }
+}
+
+   submit(){
+  this.token = this.gernerateToken()
+  this.httpClient.post("/api/validate", {
+    email: this.email,
+    password: this.password,
+    token: this.token
+  })
+  this.validate()
+}
+
+}
+
